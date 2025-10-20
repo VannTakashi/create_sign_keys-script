ee222d4d11f0d52b6cec47523c9463c854b05197
vannxavero <vannxaveroo@proton.me>
2025-10-20 12:55:05 +0700

    Adding some keys
    Signed-off-by: vannxavero <vannxaveroo@proton.me>

--- create-signed-env.sh
+++ create-signed-env.sh
@@ -31,7 +31,7 @@ clear
 echo "Press ENTER TWICE to skip password (about 10-15 enter hits total). Cannot use a password for inline signing!"
 mkdir ~/.android-certs
 
-for x in bluetooth media networkstack nfc platform releasekey sdk_sandbox shared testkey verifiedboot; do \
+for x in bluetooth media networkstack nfc platform releasekey sdk_sandbox shared testkey verifiedboot otakey nfc verity; do \
     ./development/tools/make_key ~/.android-certs/$x "$subject"; \
 done
 
